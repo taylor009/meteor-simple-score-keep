0b509b875fb31c96ebd900c71195f6345e191a5e
taylor009 <taylor@baremetalcloud.com>
2019-01-02 21:10:07 -0500

    Updated Project and removed unused code from project

--- server/main.js
+++ server/main.js
@@ -1,42 +1,7 @@
 import {Meteor} from 'meteor/meteor';
-import {Players} from './../imports/api/players';
+
 
 Meteor.startup(() => {
 
 });
 
-// // Object Spread Operator
-// let user ={
-//     name: 'Taylor',
-//     location: 'Fort Lauderdale'
-// };
-//
-// let person = {
-//     ...user,
-//     age: 20
-// };
-//
-// console.log(person);
-//
-// // Object Property Shorthand
-// let bike = 'Scott';
-// let stuff = {
-//     bike,
-//     labtop: 'Dell'
-// };
-//
-// // console.log(stuff)
-//
-// let house = {
-//     bedrooms: 4,
-//     bathrooms: 3.5
-// };
-//
-// let yearBuilt = 1996;
-//
-// console.log({
-//     ...house,
-//     bedrooms: 2,
-//     yearBuilt,
-//     flooring: 'Carpet'
-// });
